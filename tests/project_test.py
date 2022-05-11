6b3a336c809c76d7b2fc2e5722a232f4d75ce108
srb <srb23@njit.edu>
2022-05-11 13:37:20 -0400

    project pytests

--- tests/project_test.py
+++ tests/project_test.py
@@ -7,19 +7,19 @@
 6. SignUp Invalid email entry
 7. SignUp passwords doesn't match
 8. SignUp password Invalid
-9. Missing email SignUp
-10. Missing password SignUp
+9. Missing email register
+10. Missing password register
 11. Missing email signin
 12. Missing password signin
 13. Dashboard page loading successful user authentication
-14. Dashbaard User email entry.
+14. Dashboard User email entry.
 15. Manage profile -> About -> post successful
-16. Manage profile -> About -> edit sucessful
+16. Manage profile -> About -> edit successful
 17. Manage Account -> Password change successful
-18. Manage Account -> Email chnage successful
+18. Manage Account -> Email change successful
 19. Manage profile loading successful user authentication
 20. Manage Account loading successful user authentication
-21. Manage Account -> Passwords doesnt match
+21. Manage Account -> Passwords doesn't match
 22. Manage Account -> Password not valid
 23. Manage Account -> Email not valid
 24. Sudoku -> User input successful
